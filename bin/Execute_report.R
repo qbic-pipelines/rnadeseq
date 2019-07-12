1c9c741984cfe3e81d16c2e6c95329c2630ef20a
Silvia Morini <silvia.morini01@gmail.com>
2019-07-12 13:45:56 +0200

    typo

--- bin/Execute_report.R
+++ bin/Execute_report.R
@@ -20,7 +20,7 @@ qc_summary = paste('\"', opt$qc_summary, '\"', sep="")
 
 
 rmarkdown::render(opt$report, output_file = opt$output, knit_root_dir = '.', 
-                  params = list(path_qc_summary = qc_summary
+                  params = list(path_qc_summary = qc_summary,
                                 path_versions = opt$versions,
                                 path_design = opt$model,
                                 path_config = opt$config,
