d4ba61b215018c153ae8bd0b0bd10b074406ca2d
ggabernet <gisela.gabernet@qbic.uni-tuebingen.de>
2019-07-12 15:01:58 +0200

    fixed bug option read

--- bin/gProfileR.R
+++ bin/gProfileR.R
@@ -22,7 +22,7 @@ option_list = list(
   make_option(c("-d", "--dirContrasts"), type="character", default=".", help="directory with DE gene list for each contrast", metavar="character"),
   make_option(c("-m", "--metadata"), type="character", default=NULL, help="path to metadata table", metavar="character"),
   make_option(c("-m", "--model"), type="character", default=NULL, help="path to linear model file", metavar="character"),
-  make_option(c("-n", "--normCounts", type="character", default=NULL, help="path to normalized counts", metavar="character"))
+  make_option(c("-n", "--normCounts", type="character", default=NULL, help="path to normalized counts", metavar="character"),
   make_option(c("-s", "--species", type="character", default=NULL, help="Species name. Example format: Hsapiens", metavar="character"))
 )
 
