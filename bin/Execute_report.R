ea83a92181beaa2f5f26779729b6ea73a105741c
Silvia Morini <silvia.morini01@gmail.com>
2019-07-12 14:07:00 +0200

    wd path

--- bin/Execute_report.R
+++ bin/Execute_report.R
@@ -18,8 +18,10 @@ opt_parser = OptionParser(option_list=option_list)
 opt = parse_args(opt_parser)
 qc_summary_ = paste('\"', opt$qc_summary, '\"', sep='')
 
-print(paste("QC_ summary name is: ", qc_summary_, sep =""))
 print(getwd())
+wd=getwd()
+qc_summary_ = paste(wd , "/", qc_summary, sep =""))
+print(qc_summary_)
 
 rmarkdown::render(opt$report, output_file = opt$output, knit_root_dir = '.', 
                   params = list(path_qc_summary = qc_summary_,
