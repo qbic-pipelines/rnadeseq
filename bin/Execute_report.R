a1529b85565434dc8376a67a252710c41a3093ee
Silvia Morini <silvia.morini01@gmail.com>
2019-07-18 11:51:09 +0200

    took away option fastqc

--- bin/Execute_report.R
+++ bin/Execute_report.R
@@ -11,7 +11,6 @@ option_list = list(
   make_option(c("-m", "--model"), type="character", default=NULL, help="linear model file", metavar="character"),
   make_option(c("-c", "--config"), type="character", default=NULL, help="report config file", metavar="character"),
   make_option(c("-k", "--contrasts"), type="character", default=NULL, help="contrasts file", metavar="character"),
-  make_option(c("-f", "--fastqc"), type="character", default=NULL, help="fastqc reports folder", metavar="character")
 )
 
 opt_parser = OptionParser(option_list=option_list)
@@ -25,5 +24,4 @@ rmarkdown::render(opt$report, output_file = opt$output, knit_root_dir = wd, outp
                                 path_design = opt$model,
                                 path_config = opt$config,
                                 path_wd = wd,
-                                path_contrasts = opt$contrasts,
-                                path_fastqc = opt$fastqc))
+                                path_contrasts = opt$contrasts))
