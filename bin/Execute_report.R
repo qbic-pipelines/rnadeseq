28c1025f7c8d11891fc8af9c1d8ae1a5888897b6
LaurenceKuhl <laurence.kuhlburger@gmail.com>
2020-10-21 14:19:43 +0200

    Update bin/Execute_report.R
    Co-authored-by: Gisela Gabernet <gisela.gabernet@gmail.com>

--- bin/Execute_report.R
+++ bin/Execute_report.R
@@ -17,7 +17,7 @@ option_list = list(
   make_option(c("-b", "--batch_effect"), action="store_true", default=FALSE, help="Batch effect correction."),
   make_option(c("-f", "--log_FC"), type="double", default=NULL, help="Log Fold Change threshold to consider a gene DE."),
   make_option(c("-x", "--revision"), type="character", default=NULL, help="rnadeseq workflow revision", metavar="character"),
-  make_option(c("-p", "--min_DEG_pathway"), type="integer", default=NULL, help="min. number of genes DE in a pathway for this pathway to be relevant.", metavar="integer")
+  make_option(c("-p", "--min_DEG_pathway"), type="integer", default=NULL, help="min. number of genes DE in a pathway for this pathway to be considered enriched.", metavar="integer")
 )
 
 opt_parser = OptionParser(option_list=option_list)
